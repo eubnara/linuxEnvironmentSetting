78feb60b9b0fcdf5ab4a450047dfbba09fa2df02
Yubi Lee <eubnara@gmail.com>
2017-07-28 20:22:50 +0900

    Update initialize_vim.sh

--- initialize_vim.sh
+++ initialize_vim.sh
@@ -13,9 +13,6 @@ fi
 cat >> ~/.vimrc <<EOF
 " vim setting by eub
 set nocompatible " 오리지날 VI와 호환하지 않음
-set autoindent  " 자동 들여쓰기
-set cindent " C 프로그래밍용 자동 들여쓰기
-set smartindent " 스마트한 들여쓰기
 set wrap
 set nowrapscan " 검색할 때 문서의 끝에서 처음으로 안돌아감
 set nobackup " 백업 파일을 안만듬
@@ -33,7 +30,6 @@ set tabstop=4 "  탭을 4칸으로
 set lbr
 set incsearch "  키워드 입력시 점진적 검색
 syntax on "  구문강조 사용
-filetype indent on "  파일 종류에 따른 구문강조
 set background=dark " 하이라이팅 light / dark
 colorscheme desert  "  vi 색상 테마 설정
 set backspace=eol,start,indent "  줄의 끝, 시작, 들여쓰기에서 백스페이스시 이전줄로
