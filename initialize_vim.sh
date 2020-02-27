29746761f81fb3ede73fb7fb634c44d609274187
Yubi Lee <e.ub@navercorp.com>
2020-02-27 20:40:54 +0900

    Update initialize_vim.sh

--- initialize_vim.sh
+++ initialize_vim.sh
@@ -19,7 +19,7 @@ set nobackup " 백업 파일을 안만듬
 set visualbell " 키를 잘못눌렀을 때 화면 프레시
 set ruler " 화면 우측 하단에 현재 커서의 위치(줄,칸) 표시
 set shiftwidth=4 " 자동 들여쓰기 4칸
-set number " 행번호 표시, set nu 도 가능
+" set number " 행번호 표시, set nu 도 가능
 set fencs=ucs-bom,utf-8,euc-kr.latin1 " 한글 파일은 euc-kr로, 유니코드는 유니코드로
 set fileencoding=utf-8 " 파일저장인코딩
 set tenc=utf-8      " 터미널 인코딩
@@ -34,5 +34,6 @@ set background=dark " 하이라이팅 light / dark
 colorscheme desert  "  vi 색상 테마 설정
 set backspace=eol,start,indent "  줄의 끝, 시작, 들여쓰기에서 백스페이스시 이전줄로
 set history=1000 "  vi 편집기록 기억갯수 .viminfo에 기록
+set mouse-=a
 EOF
 echo "$HOME/.vimrc configuration is set."
